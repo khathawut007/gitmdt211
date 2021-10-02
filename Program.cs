edf9e38bafcfa46414ef6a1aafc47c2139b3ac2c
khathawut007 <78632160+khathawut007@users.noreply.github.com>
2021-10-03 05:53:48 +0700

    MT

--- Program.cs
+++ Program.cs
@@ -1,12 +1,84 @@
-﻿using System;
 using System.Collections.Generic;
-namespace Quiz
+using System;
+
+enum Menu {
+    Playgame = 1,
+    Exit
+}
+namespace MTT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            InforFlw Rose = Console.WriteLine("infomation:{0}",Rose);
+            PrintMenuScreen();
+            
         }
-    }
+
+        static void PrintMenuScreen() { 
+            Console.Clear();
+            PrintHeader();
+            PrintListMenu();
+            InputMenuFromKeyboard();
+        }
+
+        static void PrintHeader(){
+            Console.WriteLine("Welcome to Hangman Game");
+            Console.WriteLine("-----------------------");
+        }
+
+        static void PrintListMenu(){
+            Console.WriteLine("1.Play game");
+            Console.WriteLine("2.Exit");
+        }
+
+        static void InputMenuFromKeyboard(){
+            Console.Write("Please Select Menu: ");
+            Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+
+            PresentMenu(menu);
+        }
+
+        static void PresentMenu(Menu menu){
+            if (menu == Menu.Playgame){
+                ShowHangmanGameScreen();
+            }
+            else if (menu == Menu.Exit){
+                ShowExitScreen();
+            }else{
+                
+            }
+        }
+
+        static void ShowHangmanGameScreen(){
+            Console.Clear();
+            PrintHeaderHangmanGame();
+        }
+
+        static void PrintHeaderHangmanGame(){
+            Console.WriteLine("Play Game Hangman");
+            Console.WriteLine("--------------------");
+            
+            InCorrectcount();
+            InputHangmanLetter();
+            
+
+        }
+
+        static void InputHangmanLetter(){
+           
+           Console.Write("Input letter alphabet: {0} ");
+        }
+        static void InCorrectcount(){
+            
+            Console.WriteLine("Incorrect Score: 0 ");
+        }
+
+
+        static void ShowExitScreen(){
+            Console.Clear();
+            Console.WriteLine("Good luck!!");
+            Console.ReadLine();
+        }
+    } 
 }
